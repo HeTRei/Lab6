74351af3ab564ffa99343d8c215aae690ab4e26e
HeTRei <mememanishereboiz@gmail.com>
2023-04-13 22:12:12 +0200

    Created prompt, output functions, first steps in creating 2 random number generators which will use different seeds (different seeds not yet implemented, will probably use time(nullptr) for the m and random_device for the n

---
+++ Task1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <random>
+
+int m, k, n;
+
+std::default_random_engine Random_m(time(nullptr));
+std::default_random_engine Random_n(time(nullptr));
+void promptVariables() // Функція, що отримує від користувача значення змінних m, k, n
+{
+    std::cout << "Введіть значення 'k': ";
+    std::cin >> k;
+    if (std::cin.fail() || k < 3 || k > 10)
+    {
+        std::cout << "Неправильне значення! Повинно бути цілим числом від 3 до 10 включно. Завершення роботи...";
+        std::exit(1);
+    }
+    std::cout << "Введіть значення 'm': ";
+    std::cin >> m;
+    if (std::cin.fail() || m % k != 0)
+    {
+        std::cout << "Неправильне значення! Повинно бути цілочисленним та ділитися на k без залишку. Завершення роботи...";
+        std::exit(2);
+    }
+
+    std::cout << "Введіть значення 'n': ";
+    std::cin >> n;
+    if (std::cin.fail() || n % k != 0)
+    {
+        std::cout << "Неправильне значення! Повинно бути цілочисленним та ділитися на k без залишку. Завершення роботи...";
+        std::exit (3);
+    }
+
+}
+void printRandom_m() // Функція, що виводить рандомні значення 'm' цілих чисел в проміжку від 13 до 399 включно
+{
+    for (int total, column = 0; total < m; total += column)
+    {
+        for (column = 0; column <= k; column++)
+            std::cout << Random_m();
+
+    }
+}
+void printRandom_n() // Функція, що виводить випадкові значення 'n' чисел в проміжку від 13 до 399 включно
+{
+    for (int total, column = 0; total < n; total += column)
+    {
+        for (column = 0; column <= k; column++)
+            std::cout << Random_n();
+    }
+}
+
+int main()
+{
+    std::setlocale (LC_CTYPE, "ua");
+    promptVariables();
+    return 0;
+}
